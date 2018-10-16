45da2078a619fb0dc99dadc3c3e942539d30eccd
Roman <smakula2012@gmail.com>
2018-10-16 21:15:47 +0300

    create first version
    Add layout, view model, retrofit, data binding

---
+++ app/src/main/java/com/example/roman/test/Validator.kt
@@ -0,0 +1,45 @@
+package com.example.roman.test
+
+import java.security.MessageDigest
+
+class Validator {
+
+    object Bitcoin {
+        private const val ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+        private fun ByteArray.contentEquals(other: ByteArray): Boolean {
+            if (this.size != other.size) return false
+            return (0 until this.size).none { this[it] != other[it] }
+        }
+
+        private fun decodeBase58(input: String): ByteArray? {
+            val output = ByteArray(25)
+            for (c in input) {
+                var p = ALPHABET.indexOf(c)
+                if (p == -1) return null
+                for (j in 24 downTo 1) {
+                    p += 58 * (output[j].toInt() and 0xff)
+                    output[j] = (p % 256).toByte()
+                    p = p shr 8
+                }
+                if (p != 0) return null
+            }
+            return output
+        }
+
+        private fun sha256(data: ByteArray, start: Int, len: Int, recursion: Int): ByteArray {
+            if (recursion == 0) return data
+            val md = MessageDigest.getInstance("SHA-256")
+            md.update(data.sliceArray(start until start + len))
+            return sha256(md.digest(), 0, 32, recursion - 1)
+        }
+
+        fun validateAddress(address: String): Boolean {
+            if (address.length !in 26..35) return false
+            val decoded = decodeBase58(address)
+            if (decoded == null) return false
+            val hash = sha256(decoded, 0, 21, 2)
+            return hash.sliceArray(0..3).contentEquals(decoded.sliceArray(21..24))
+        }
+    }
+}
